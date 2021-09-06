49455df37db7c25e737289b7f9d18e23a44fa510
Sanjeev94 <sanjeev.dubey930@gmail.com>
2021-09-06 15:39:19 +0530

    add button

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const billAmount = document.querySelector(#bill-amount);
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("check-button");
+console.log(billAmount.value);
+console.log(cashGiven.value);
